9968054d42bd478c68bb390c6c099d4342a398ac
zetra <karanpatadia28@gmail.com>
2024-11-12 15:24:33 +0530

    Adding code

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import CustomerReviews from './CustomerReviews';
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  Footer,
+  SpecialOffer,
+  Subscribe,
+  CustomerReviews,
+};
